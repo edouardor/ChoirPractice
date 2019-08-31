f51a992e7dbe78103570cfdeadaebb01b4c8e20e
michaelbryan <michaelbryan.m@gmail.com>
2019-08-31 13:42:13 -0600

    Some more test scripts.

--- fs_note.py
+++ fs_note.py
@@ -1,18 +1,34 @@
 import time
 import fluidsynth
 
+# First a Synth object is created to control playback.
 fs = fluidsynth.Synth()
-fs.start()
+# The start() method starts audio output in a separate thread.
+# fs.start()
+fs.start(driver="alsa")
+## Your installation of FluidSynth may require a different driver.
+## Use something like:
+# fs.start(driver="pulseaudio")
+# fs.start(driver="alsa", midi_driver="alsa_seq")
 
-sfid = fs.sfload("example.sf2")
+# Got this error trying different drivers:
+# fluidsynth: error: Couldn't find the requested 
+# audio driver portaudio. Valid drivers are: 
+# alsa, file, jack, oss, pulseaudio.
+
+# sfid = fs.sfload("example.sf2")
+sfid = fs.sfload("/usr/share/sounds/sf2/FluidR3_GM.sf2")
+# program_select(track, soundfontid, banknum, presetnum)
 fs.program_select(0, sfid, 0, 0)
 
+# noteon(track, midinum, velocity)
 fs.noteon(0, 60, 30)
 fs.noteon(0, 67, 30)
 fs.noteon(0, 76, 30)
 
 time.sleep(1.0)
 
+# noteoff(track, midinum)
 fs.noteoff(0, 60)
 fs.noteoff(0, 67)
 fs.noteoff(0, 76)
